af02fe665e0e503fbf2fbe60b7c1c1a7aa1527fe
Naz <elik@example.com>
2023-06-28 21:15:35 +0400

    Homework 1_4

---
+++ lesson_1/1_4/Program.cs
@@ -0,0 +1,25 @@
+﻿int max = 0;
+
+Console.Write("Введите первое число: ");
+int firstNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int secondNumber = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int thirdNumber = int.Parse(Console.ReadLine()!);
+
+if(firstNumber > max)
+{
+    max = firstNumber;
+}
+if(secondNumber > max)
+{
+    max = secondNumber;
+}
+if(thirdNumber > max)
+{
+    max = thirdNumber;
+}
+
+Console.WriteLine("max = " + max);
