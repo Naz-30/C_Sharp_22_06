e9bc30f8cc3be81214a2e1cab819ea44321f50d2
Naz <elik@example.com>
2023-06-28 14:44:54 +0400

    Homework 1_2

---
+++ lesson_1/1_2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Введите первое число: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.WriteLine("Введите второе число: ");
+int b = int.Parse(Console.ReadLine()!);
+if (a>b)
+{
+    Console.Write("Первое число больше второго");
+    }
+else if (a < b)
+            {
+                Console.WriteLine("Первое число меньше второго");
+            }
+else {
+    Console.Write("Оба числа равны");
+}
