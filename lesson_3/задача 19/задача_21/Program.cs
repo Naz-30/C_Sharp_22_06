69c0ee2de89cd9639628468f5110add147061482
Naz <elik@example.com>
2023-07-06 13:25:51 +0400

    Homework_num21

---
+++ lesson_3/задача 19/задача_21/Program.cs
@@ -0,0 +1,14 @@
+﻿double Line3D(int x1, int y1, int z1, int x2, int y2, int z2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2-x1, 2)+Math.Pow(y2-y1,2)+Math.Pow(z2-z1,2)),2);
+}
+int x_1=int.Parse(Console.ReadLine()!);
+int y_1=int.Parse(Console.ReadLine()!);
+int z_1=int.Parse(Console.ReadLine()!);
+int x_2=int.Parse(Console.ReadLine()!);
+int y_2=int.Parse(Console.ReadLine()!);
+int z_2=int.Parse(Console.ReadLine()!);
+
+double result = Line3D(x_1, y_1, z_1,
+                       x_2, y_2, z_2);
+Console.WriteLine(result);
