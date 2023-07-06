ee8cce276f1ed0358d2f0ad308133c4017ccceab
Naz <elik@example.com>
2023-07-06 13:37:42 +0400

    Homework_num23

---
+++ lesson_3/задача 19/задача_23/Program.cs
@@ -0,0 +1,14 @@
+﻿void Squares(int n)
+{
+    double[] nums=new double[n];
+    int index=0;
+    Console.WriteLine($"{n}->");
+    while(index<n)
+    {
+        nums[index]=Math.Pow((index+1), 3);
+        Console.Write($"{nums[index]}, ");
+        index +=1;
+            }
+        Console.WriteLine();
+}
+Squares(int.Parse(Console.ReadLine()!));
