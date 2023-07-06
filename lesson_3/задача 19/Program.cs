29d39d9171b7063dba32cc70b852b743fea0b3ea
Naz <elik@example.com>
2023-07-06 13:07:56 +0400

    Homework_num19

---
+++ lesson_3/задача 19/Program.cs
@@ -0,0 +1,14 @@
+﻿void Pali(int num)
+{
+    int num1=num/1000;
+    int num2=num%10;
+    int num3=num/10%10;
+
+    if(num1==num2*10+num3)
+        Console.WriteLine($"Yes, {num} is a palindrome");
+    else
+        Console.WriteLine($"No, {num} is not a palindrome");
+}
+Console.WriteLine("Enter a five-digit number");
+int val=int.Parse(Console.ReadLine()!);
+Pali(val);
