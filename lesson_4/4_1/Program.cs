3ac386ed57d0dafae0816ed80e18607690a2163f
Naz <elik@example.com>
2023-07-10 17:41:37 +0400

    Homework 4_1

---
+++ lesson_4/4_1/Program.cs
@@ -0,0 +1,31 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput = System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int Power(int powerBase, int exponent)
+{
+    int power = 1;
+    for (int i = 0; i < exponent; i++)
+    {
+        power *= powerBase;
+    }
+    return power;
+}
+bool ValidateExponent(int exponent)
+{
+    if (exponent < 0)
+    {
+        System.Console.WriteLine("Показатель не должен быть меньше 0");
+        return false;
+    }
+    return true;
+}
+int powerBase = Prompt("Введите основание: ");
+int exponent = Prompt("Введите показатель: ");
+if (ValidateExponent(exponent))
+{
+    System.Console.WriteLine($"Число {powerBase} в степени {exponent} равно {Power(powerBase, exponent)}");
+}
