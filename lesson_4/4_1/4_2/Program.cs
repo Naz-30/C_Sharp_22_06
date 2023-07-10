05265704a821bc76e230cf117c86d2fb24abdce9
Naz <elik@example.com>
2023-07-10 18:20:46 +0400

    Homework 4_2

---
+++ lesson_4/4_1/4_2/Program.cs
@@ -0,0 +1,20 @@
+﻿int Prompt(string message)
+{
+    System.Console.Write(message);
+    string readInput=System.Console.ReadLine();
+    int result = int.Parse(readInput);
+    return result;
+}
+int SmallDigit(int number)
+{
+    int result = 0;
+    while (number>0)
+    {
+        result +=number%10;
+        number=number/10;
+    }
+    return result;
+}
+
+int number = Prompt("Введите число:");
+System.Console.WriteLine($"Сумма всех чисел в цифре {number} = {SmallDigit(number)}");
